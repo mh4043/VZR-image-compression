a024585f41ec8984b952b8d567a39b159f098db2
vc4183 <vc4183@student.uni-lj.si>
2023-05-30 15:59:39 +0200

    OMP-oddaja

---
+++ OMP/openMP-oddaja/first_tests_batch_VC_compress_MP.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#SBATCH --reservation=fri
+#SBATCH --job-name=openMP_compress_first
+#SBATCH --output=first_log_vc_compress.log
+#SBATCH --time=01:30:00
+#SBATCH --constraint=AMD         #? vozlišča z AMD procesorji
+#SBATCH --ntasks=1               #? ena instanca programa
+#SBATCH --cpus-per-task=64       #? koliko proc jeder dodelimo nalogi --> ce to zelimo izkoristit mora bit OMP_NUM_THREADS enaka stevilka
+#SBATCH --wait
+
+
+
+#Multithreading
+export OMP_PLACES=cores      #? razporejanje niti -> niti želimo razporediti na jedra, ne na sockete ##moznosti socket (fizicni procesor), threads (vsaka nit dobi svojo strojno nit hyperthreading 1 jedro več niti), cores jedro
+export OMP_PROC_BIND=TRUE    #? ko se nit zažene na jedru tam tudi ostane
+export OMP_NUM_THREADS=64    #? koliko niti bi želeli uporabit, lahko se znotraj pograma nastavi, ampak pol treba ponovno prevajat, tu pa z okoljsko spremenljivko zamenjamo
+
+
+
+
+
+srun VC_compress_MP livingRoom.png 8 100
+srun VC_compress_MP livingRoom.png 16 100
+srun VC_compress_MP livingRoom.png 32 100
+srun VC_compress_MP livingRoom.png 64 100
+srun VC_compress_MP livingRoom.png 128 100
+
+
