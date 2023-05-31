cfa80638daa6a7f25d755e7c6f16615fc6ad95a5
MT <61147812+mh4043@users.noreply.github.com>
2023-05-31 09:12:25 +0200

    Update mpi_knn

--- MPI/run.sh
+++ MPI/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# use: srun --reservation=fri --mpi=pmix -n<number of processes> -N<number of nodes> mpi_knn <path to image> <number of clusters> <number of iterations>
 module load OpenMPI/4.1.0-GCC-10.2.0
 mpicc mpi_knn.c -lm -o mpi_knn
-srun --reservation=fri --mpi=pmix -n4 -N1 mpi_knn ../images/lena.png 32 1
+srun --reservation=fri --mpi=pmix -n32 -N1 mpi_knn ../images/living_room.png 32 100 >> mpi_kmeans_results.txt
