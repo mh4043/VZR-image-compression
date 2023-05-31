c99afb2c9175be74e656face13646403425473ae
MT <61147812+mh4043@users.noreply.github.com>
2023-05-31 09:11:41 +0200

    Update mpi_knn

--- MPI/mpi_knn.c
+++ MPI/mpi_knn.c
@@ -14,29 +14,22 @@ int main(int argc, char *argv[]) {
     // MPI vars
     int rank; // process rank 
 	int num_p; // total number of processes 
-	int source; // sender rank
-	int tag = 0; // message tag
-	int buffer[1]; // message buffer 
-	MPI_Status status; // message status
-	MPI_Request request; // MPI request
-	int flag; // request status flag
     
     unsigned int *clusters_centroids; // centroids = [r1, g1, b1, a1, r2, g2, b2, a2, ...]
     int width, height, cpp;  // Image qualities
     int img_qualities[3]; // width, height, cpp
     unsigned char *image_original, *image_original_part; // image_original_part is for individual process
-    // Function parameters
     char *image_file;
-    int number_of_clusters, number_of_iterations;
-    int block_size;
-    int *sendcounts, *recvcounts_centr_index; // sizes of blocks for each process
-	int *displs, *displs_centr_index; // offsets in array for each process
-    int noOfPixels;
+    int number_of_clusters, number_of_iterations; // number of clusters = number of colors, we get both values from input
+    int block_size; // size of a block of pixels that go to one process
+    int *sendcounts, *recvcounts_centr_index; // sizes of blocks for each process (used in Scatter and Gather)
+	int *displs, *displs_centr_index; // offsets in array for each process (used in Scatter and Gather)
+    int noOfPixels; // number of pixels that each process gets (last process might have different number)
     int *pixel_centroid_index; // array that holds index of the closest centroid to each pixel (per process)
-    int *all_pixel_centroid_index; // array that holds indexe of the closest centroid to each pixel from all processes
-    long *partial_avg; // sum/partial avg of pixel RGBA when updating centroids for individual centroids
-    long *averages; //complete averages of RGBA for new centroid
-    int count; // count of pixels when updating centroids for individual centroids
+    long *partial_sum; // sum of pixel RGBA when updating centroids for individual centroids (partial sum has +1 space for count - comes in effect when reduce happens)
+    long *sums; //complete sums of RGBA for new centroid
+    long count, partial_count; // count of pixels when updating centroids for individual centroids
+    double initTime, totalTime;
 
     srand(time(NULL));
 
@@ -57,7 +50,8 @@ int main(int argc, char *argv[]) {
     if(rank == 0){
         // read image and its dimensions and cpp
         image_file = argv[1];
-        printf("Starting program with parameters %s, %d, %d\n", image_file, number_of_clusters, number_of_iterations);
+        printf("Starting program with parameters: %s, %d, %d\n", image_file, number_of_clusters, number_of_iterations);
+        printf("Number of processes: %d\n", num_p);
         fflush(stdout);
 
         image_original = stbi_load(image_file, &width, &height, &cpp, 0);
@@ -72,8 +66,6 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Error loading image %s!\n", image_file);
             exit(1);
         }
-
-        all_pixel_centroid_index = (int *)malloc(width * height * sizeof(int));
     }
 
     // broadcast image qualities (width, height, cpp) to all other processes (Bcast is blocking comm, so we don't have to use barrier)
@@ -84,8 +76,8 @@ int main(int argc, char *argv[]) {
     cpp = img_qualities[2];
 
     clusters_centroids = (unsigned int *)malloc(number_of_clusters * cpp * sizeof(unsigned int));  // An array of cluster centroids (each cluster has one pixel (RGBA))
-    partial_avg = (long *)malloc(cpp * sizeof(long));
-    averages = (long *)malloc(cpp * sizeof(long));
+    partial_sum = (long *)malloc((cpp + 1) * sizeof(long)); // +1 so that when reducing, count is also in this array
+    sums = (long *) malloc((cpp + 1) * sizeof(long));
 
     // setting up for Scatterv for pixels
     sendcounts = (int*)malloc(num_p * sizeof(int)); // number of elements to send to each process
@@ -94,8 +86,8 @@ int main(int argc, char *argv[]) {
     displs_centr_index = (int*)malloc(width * height * sizeof(int));   
     int b_size_part = ceil((width * height * cpp) / (num_p * 1.0));
     block_size = b_size_part + (b_size_part % cpp); // size of one block to go to one process (we want it to be divisible by cpp - so that it can't happent that p1 gets red of a pixel and p2 gets green of the same pixel)
-    int last_process_block_size = (width * height * cpp) - (block_size * (num_p - 1));
-        for(int i = 0; i < num_p; i ++){
+    int last_process_block_size = (width * height * cpp) - (block_size * (num_p - 1)); // last block might have a different size
+    for(int i = 0; i < num_p; i ++){
         displs[i] = i * block_size;
         displs_centr_index[i] = i * (block_size / cpp);
         if(i + 1 >= num_p){
@@ -112,14 +104,19 @@ int main(int argc, char *argv[]) {
         noOfPixels = last_process_block_size / cpp;
     }
     image_original_part = (unsigned char*)malloc(block_size * sizeof(unsigned char));
+    if(rank == num_p - 1){
+        image_original_part = (unsigned char*)malloc(last_process_block_size * sizeof(unsigned char));
+        block_size = last_process_block_size;
+    }
     // scatterv pixels of an image to all processes
     MPI_Scatterv(image_original, sendcounts, displs, MPI_UNSIGNED_CHAR, image_original_part, block_size, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    
     MPI_Barrier(MPI_COMM_WORLD);
 
     pixel_centroid_index = (int *)malloc(noOfPixels * sizeof(int));
 
     if(rank == 0){
-        // Init clusters as 1D array
+        // Init clusters as 1D array ([r1, g1, b1, (a1), r2, b2, g2, (a2), ...])
         for (int i = 0; i < number_of_clusters; i++) {
             int random_pixel_w = (i * width) / number_of_clusters;
             int random_pixel_h = (i * height) / number_of_clusters;
@@ -127,12 +124,20 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < cpp; j++) {
                 clusters_centroids[i * cpp + j] = image_original[(random_pixel_h * width + random_pixel_w) * cpp + j];
             }
-            // printf("X and Y for cluster: %d, %d, \n", random_pixel_w, random_pixel_h);
         }   
     }
 
-    for(int iter = 0; iter < number_of_iterations; iter ++){
+    // start the timer
+    if(rank == 0){
+        initTime = MPI_Wtime();
+    }
 
+    for(int iter = 0; iter < number_of_iterations; iter ++){
+        if(rank == 0){
+            printf("ITERATION: %d\n", iter);
+            fflush(stdout);
+        }
+        
         // broadcast cluster centroids array to all other processes
         MPI_Bcast(clusters_centroids, number_of_clusters * cpp, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
 
@@ -148,11 +153,11 @@ int main(int argc, char *argv[]) {
                 for (int j = 0; j < cpp; j++) {
                     char cntr = clusters_centroids[centroid * cpp + j];
                     char delta = image_original_part[pxl * cpp + j] - cntr;
-                    dst += powf(delta, 2);
+                    dst += (delta * delta);
                 }
 
                 // Get euclidean distance
-                dst = sqrtf(dst);
+                // dst = sqrtf(dst);
 
                 // Check if distance is smaller than min_dst
                 if (dst < min_dst) {
@@ -163,58 +168,61 @@ int main(int argc, char *argv[]) {
             pixel_centroid_index[pxl] = min_indx;
         }
 
-        // gathers pixels centroid indexes from all processes
-        // MPI_Gatherv(pixel_centroid_index, noOfPixels, MPI_INT, all_pixel_centroid_index, recvcounts_centr_index, displs_centr_index, MPI_INT, 0, MPI_COMM_WORLD);
-
         MPI_Barrier(MPI_COMM_WORLD);
 
-        // 2. For each centroid compute the mean value of all pixels, where pixels' centroid index (from all_pixel_centroid_index) is current centroid index
+        if(rank == 0){
+            printf("ITERATION: %d, 50\%\n", iter);
+            fflush(stdout);
+        }
+
+        // 2. For each centroid compute the mean value of all pixels, where pixels' centroid index is current centroid index
         //    And then set the centroid value to the mean value.
-        // parallelize: 1. the outer loop - each process checks a certain number of centroids OR 2. the inner loop (outer loop is serial) - each process calculates a portion of a mean value and then sums it together
-        //    2. the inner loop - when one centroid is fully searched, MPI_Reduce is used to SUM all the partial means
-        //       each process gets (block_size / cpp) pixels to go over and calculate the partial mean based on current centroid (pixel_centroid_index)
-        
+        //    parallelize: 
+        //        - the inner loop - when one centroid is fully searched, MPI_Reduce is used to SUM all the partial sums and calculate its mean
+        //        - each process gets (block_size / cpp) pixels to go over and calculate the partial sum based on current centroid (pixel_centroid_index)
+        //          then, reduce is called with operation SUM, to sum all red colors, green colors, blue colors and counts individually from all processes
+        //          aftr that, root proces checks if there were no pixels for the current centroid - in this case, a random pixel is chosen from the image, as its value
+        //          else, calculates the mean value of each color and updates centroid values with the mean ones
         for(int centroid = 0; centroid < number_of_clusters; centroid++){
-            // reset partial avg values
+            // reset partial sum values
             for(int j = 0; j < cpp; j++){
-                partial_avg[j] = 0;
+                partial_sum[j] = 0;
             }
-            count = 0;
+            partial_count = 0;
+
+            // go through all pixels and check if current centroid corresponds to pixel centroid
             for(int pxl = 0; pxl < noOfPixels; pxl ++){
                 if(centroid == pixel_centroid_index[pxl]){
                     // sum values for each of the RGBA channels
                     for(int j = 0; j < cpp; j++){
-                        partial_avg[j] += image_original_part[pxl * cpp + j];
+                        partial_sum[j] += image_original_part[pxl * cpp + j];
                     }
-                    count += 1;
+                    partial_count += 1;
                 }
             }
 
-            // if no pixels in cluster - put random value in centroid (TODO)
-            if(count == 0){
-                continue;
-            }
-
-            for(int j = 0; j < cpp; j++){
-                partial_avg[j] = round(partial_avg[j] / count);
-            }
-
-            // reduce partial averages of processes with sum into averages and replace centroid values with these averaged ones
-            MPI_Reduce(partial_avg, averages, cpp, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+            // the last element in this array is the number of pixels that belong to current centroid
+            partial_sum[cpp] = partial_count;
+        
+            // reduce partial sums and counts of processes with sum into sums and calculate averages. Replace centroid values with these averaged ones
+            MPI_Reduce(partial_sum, sums, cpp + 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
             if(rank == 0){
-                for(int j = 0; j < cpp; j++){
-                    clusters_centroids[centroid * cpp + j] = averages[j];
+                count = sums[cpp]; // extract the count (last element)
+                // if count is 0, assign a random pixel as centroid to that (current) cluster, else average and replace
+                if(count == 0){
+                    int random_pxl = rand() % (width * height);
+                    for(int j = 0; j < cpp; j++){
+                        clusters_centroids[centroid * cpp + j] = image_original[random_pxl * cpp + j];
+                    }
+                }else{
+                    for(int j = 0; j < cpp; j++){
+                        clusters_centroids[centroid * cpp + j] = round(sums[j] / count);
+                    }
                 }
             }
 
             MPI_Barrier(MPI_COMM_WORLD);
-        }
-
-        if(rank == 0){
-            for(int i = 0; i < number_of_clusters; i++){
-                printf("i: %d, new cluster values: R %d G %d B %d\n", i, clusters_centroids[i * cpp], clusters_centroids[i * cpp + 1], clusters_centroids[i * cpp + 2]);
-            }
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
@@ -223,16 +231,31 @@ int main(int argc, char *argv[]) {
     // overwrite pixel values with cluster centroid values
     MPI_Bcast(clusters_centroids, number_of_clusters * cpp, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
 
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // each process overwrites its part of an image with cluster centroid values
     for (int pxl = 0; pxl < noOfPixels; pxl ++){
         for (int j = 0; j < cpp; j++) {
             image_original_part[pxl * cpp + j] = clusters_centroids[pixel_centroid_index[pxl] * cpp + j];
         }
-    }    
-
-    // gather every partial image back into full image
-    MPI_Gatherv(image_original_part, block_size, MPI_UNSIGNED_CHAR, image, sendcounts, displs, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    }
 
     MPI_Barrier(MPI_COMM_WORLD);
+
+    // gather every partial image back into full image
+    if(rank == num_p - 1){
+        block_size = last_process_block_size;
+    }
+    MPI_Gatherv(image_original_part, block_size, MPI_UNSIGNED_CHAR, image_original, sendcounts, displs, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // stop the timer
+    if(rank == 0){
+        totalTime = MPI_Wtime() - initTime;
+        printf("MPI time: %.3f\n", totalTime);
+        fflush(stdout);
+    }
 
     // output image
     if(rank == 0){
@@ -257,14 +280,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
     free(clusters_centroids);
     free(image_original_part);
-    free(image);
+    free(image_original);
     
     MPI_Finalize(); 
-
 
     return 0;
 }
